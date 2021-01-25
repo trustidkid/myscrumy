ba8ea4f29f8021343abdf610ded6fc811d11fd95
trustidkid <yemi.bili07@gmail.com>
2021-01-25 13:22:42 +0100

    modified application name

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('semiu.urls')),
+    path('',include('semiuscrumy.urls')),
 ]
 
 
--- semiuscrumy/urls.py
+++ semiuscrumy/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from semiu import views
+from semiuscrumy import views
 urlpatterns = [
     path('', views.index)
 ]
