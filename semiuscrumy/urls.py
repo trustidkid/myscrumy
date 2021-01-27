5453b29cc6b1ecc180193f8aa42710ba9304a151
trustidkid <yemi.bili07@gmail.com>
2021-01-27 13:12:43 +0100

    modified the url patterns

--- semiuscrumy/urls.py
+++ semiuscrumy/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from semiuscrumy import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.get_grading_parameters)
 ]
--- semiuscrumy/views.py
+++ semiuscrumy/views.py
@@ -5,5 +5,3 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 def get_grading_parameters(request): 
     return HttpResponse("Welcome to Django")
-def index(request):
-    return HttpResponse("You are welcome to Django")
