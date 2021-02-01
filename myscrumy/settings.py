d881c5b6f4eebdd4ffff9c9c390b324f3fc35031
trustidkid <yemi.bili07@gmail.com>
2021-02-01 14:19:56 +0100

    modified host ip

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_l0s6(xbv0qzp!*r9cinni9-rcwnb53&o7ihv-03_j#dn&+m4k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.212.207.214']
+ALLOWED_HOSTS = ['34.221.162.133']
 
 
 # Application definition
