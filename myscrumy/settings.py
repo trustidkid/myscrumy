936967ea85505af8f87eec54a820a433aecbb047
trustidkid <yemi.bili07@gmail.com>
2021-02-04 18:20:09 +0100

    modified db password

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'semiuscrumy', #BASE_DIR / 'db.sqlite3'
         'USER': 'root',
-        'PASSWORD': ''#'beurself07'
+        'PASSWORD': 'beurself07'
     }
 }
 
