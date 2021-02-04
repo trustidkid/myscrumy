e0294109caa90b133fba718a03ff48b088e56023
trustidkid <yemi.bili07@gmail.com>
2021-02-04 12:08:19 +0100

    added mysql database settings

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-        #'USER': 'root',
-        #'PASSWORD': ''
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'semiuscrumy', #BASE_DIR / 'db.sqlite3'
+        'USER': 'root',
+        'PASSWORD': 'beurself07'
     }
 }
 
