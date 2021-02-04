2ea1141366b53e5f8b0c579ed294ca580a597a29
trustidkid <yemi.bili07@gmail.com>
2021-02-04 18:17:56 +0100

    added models

--- semiuscrumy/models.py
+++ semiuscrumy/models.py
@@ -7,8 +7,6 @@ from django.contrib.auth.models import User
 class GoalStatus(models.Model):
     scrumy_name = models.CharField(max_length=200)
 
-    #class Meta:
-       # app_label = 'semiuscrumy'
 
     def __str__(self):
         return self.scrumy_name
