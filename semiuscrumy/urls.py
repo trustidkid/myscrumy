7b424cc6c567adb0813a1106645847d80652a947
trustidkid <yemi.bili07@gmail.com>
2021-02-05 18:58:44 +0100

    modify the view retrieve data from db

--- semiuscrumy/urls.py
+++ semiuscrumy/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_grading_parameters)
+    #path('movegoal/<int:goal_id>',views.move_goal, name="movegoal")
 ]
--- semiuscrumy/views.py
+++ semiuscrumy/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from semiuscrumy.models import ScrumyGoals, ScrumyHistory, GoalStatus
+from .models import ScrumyGoals, GoalStatus
 
 # Create your views here.
-def get_grading_parameters(request): 
-    return HttpResponse("This is a Scrum Application")
+
+def get_grading_parameters(request):
+    goal = ScrumyGoals.objects.get(goal_name="Learn Django")
+    return HttpResponse(goal)
+
+
+#def move_goal(request, goal_id):
+    #goal = ScrumyGoals.objects.get(goal_name="Learn Django")
+    #goalname = response.goal_name
+    #return HttpResponse(goal)
