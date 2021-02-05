6a41ea7aba0c131ec26933ee1bd2695b3427ed7e
trustidkid <yemi.bili07@gmail.com>
2021-02-05 20:00:21 +0100

    removes limitation from charfield

--- semiuscrumy/models.py
+++ semiuscrumy/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class GoalStatus(models.Model):
-    scrumy_name = models.CharField(max_length=200)
+    scrumy_name = models.CharField()
 
 
     def __str__(self):
@@ -14,11 +14,11 @@ class GoalStatus(models.Model):
 class ScrumyGoals(models.Model):
     #goal_name, goal_id, created_by, moved_by, owner
     goal_id = models.BigIntegerField()
-    goal_name = models.CharField(max_length=200)
-    created_by = models.CharField(max_length=200)
-    moved_by = models.CharField(max_length=200)
-    owner = models.CharField(max_length=200)
-    goal_status = models.ForeignKey(GoalStatus,on_delete=models.PROTECT)
+    goal_name = models.CharField()
+    created_by = models.CharField()
+    moved_by = models.CharField()
+    owner = models.CharField()
+    goal_status = models.ForeignKey(GoalStatus,on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='scrumygoals')
     
     def __str__(self):
@@ -27,12 +27,12 @@ class ScrumyGoals(models.Model):
 
 class ScrumyHistory(models.Model):
     #moved_by, created_by, moved_from, moved_to, time_of_action
-    created_by = models.CharField(max_length=200)
-    moved_by = models.CharField(max_length=200)
-    moved_from = models.CharField(max_length=200)
-    moved_to = models.CharField(max_length=200)
-    time_of_action = models.CharField(max_length=200)
-    goal = models.ForeignKey(ScrumyGoals, on_delete=models.PROTECT)
+    created_by = models.CharField()
+    moved_by = models.CharField()
+    moved_from = models.CharField()
+    moved_to = models.CharField()
+    time_of_action = models.CharField()
+    goal = models.ForeignKey(ScrumyGoals, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.created_by
