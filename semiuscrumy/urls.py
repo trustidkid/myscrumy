63cc046f51864580f551c36b18ee1ccf1ac7f5d4
trustidkid <yemi.bili07@gmail.com>
2021-02-10 12:34:39 +0100

    refactored url name

--- semiuscrumy/urls.py
+++ semiuscrumy/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.get_grading_parameters),
     path('/movegoal/<int:goal_id>', views.move_goal),
-    path('/add_goal', views.add_goal),
+    path('/addgoal', views.add_goal),
     path('/home', views.home)
 ]
