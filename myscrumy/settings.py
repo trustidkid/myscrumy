db15728a69c88d20eaaaaf4b6e0e9290a34521f7
trustidkid <yemi.bili07@gmail.com>
2021-03-01 14:49:10 +0100

    modified databse connection

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_l0s6(xbv0qzp!*r9cinni9-rcwnb53&o7ihv-03_j#dn&+m4k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.236.27.185']
+ALLOWED_HOSTS = ['100.25.22.142']
 
 
 # Application definition
