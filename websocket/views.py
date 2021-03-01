7ac59c99d4e43456f6c80f9924057b25000dfae1
trustidkid <yemi.bili07@gmail.com>
2021-03-01 17:19:04 +0100

    modified view code for connect to retun json

--- websocket/views.py
+++ websocket/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from websocket.models import ChatMessage, Connection
 import json
+import boto3
 
 # Create your views here.
 @csrf_exempt #to ignore csrf token
@@ -21,14 +22,35 @@ def connect(request):
     connection_id = body["connectionId"] 
     savecon = Connection(connection_id=connection_id)
     savecon.save()
-    JsonResponse({"message":"connect successfully"}, status=200)
+    return JsonResponse({"message":"connect successfully"}, status=200)
 
 @csrf_exempt
 def disconnect(request):
     body = _parse_body(request.body)
     connection_id = body["connectionId"] 
-    savecon = Connection(connection_id=connection_id)
-    savecon.delete()
-    JsonResponse({"message":"disconnect successfully"}, status=200)
+    deletecon = Connection(connection_id=connection_id)
+    deletecon.delete()
+    return JsonResponse({"message":"disconnect successfully"}, status=200)
+    
+#def _send_to_connection(connection_id, data):
+    #gatewayapi = boto3.client(‘apigatewaymanagementapi’, endpoint_url='https://uvy5wo0wg4.execute-api.us-east-1.amazonaws.com/testStage/@connections',region_name='us-east-2', aws_access_key_id = 'AKIAIWIKCOMA7AZMVQHQ', aws_secret_access_key = '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')
+    #Using boto3 to make post request to api gateway
+    #return gatewayapi.post_to_connection(ConnectionId=connection_id, data="json.dumps(data).encode('utf-8'")
+
+#sending message
+
+def send_message(request):
+    """
+    * passed the request body to a new variable called body * save the content, username and timestamp from the body dictionary to database using the ChatMessage model * get all the conection_id from the database using Connection model and assign it to a new variable called connections * prepare body as a list of messages, then assign it to a data variable(data = {‘messages’:[body]}) * loop over connections and use _send_to_connection function to send data to all connected client """
+    body = _parse_body(request.body)
+    username = body['username']
+    timestamps = body['timestamp']
+    message = body['message']
+    save_to_db = ChatMessage(username=username, message = message, timestamp=timestamps)
+    connections = Connection.objects.all()
+    data = {'messages':[body]}
+    for con in connections:
+        _send_to_connection(con.id, data)
+    save_to_db.save()
     
     
